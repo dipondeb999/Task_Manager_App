037cfe8c4e8fad292949e62e7e9cfd2d781811ca
dipondeb <developerdipon@gmail.com>
2024-10-07 01:26:08 +0600

    updated

--- lib/ui/screens/sign_up_screen.dart
+++ lib/ui/screens/sign_up_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:task_manager_app/data/models/network_response.dart';
+import 'package:task_manager_app/data/services/network_caller.dart';
+import 'package:task_manager_app/data/utils/urls.dart';
 import 'package:task_manager_app/ui/utils/app_colors.dart';
+import 'package:task_manager_app/ui/widgets/centered_circular_progress_indicator.dart';
 import 'package:task_manager_app/ui/widgets/screen_background.dart';
+import 'package:task_manager_app/ui/widgets/snack_bar_message.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -13,11 +18,11 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   bool _inProgress = false;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  TextEditingController _emailTEController = TextEditingController();
-  TextEditingController _firstNameTEController = TextEditingController();
-  TextEditingController _lastNameTEController = TextEditingController();
-  TextEditingController _mobileTEController = TextEditingController();
-  TextEditingController _passwordTEController = TextEditingController();
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _firstNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final TextEditingController _mobileTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
@@ -54,43 +59,83 @@ class _SignUpScreenState extends State<SignUpScreen> {
         children: [
           TextFormField(
             controller: _emailTEController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             keyboardType: TextInputType.emailAddress,
             decoration: const InputDecoration(
               hintText: "Email",
             ),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter valid email';
+              }
+              return null;
+            },
           ),
           const SizedBox(height: 8),
           TextFormField(
             controller: _firstNameTEController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             decoration: const InputDecoration(
               hintText: "First name",
             ),
-          ),const SizedBox(height: 8),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter first name';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 8),
           TextFormField(
             controller: _lastNameTEController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             decoration: const InputDecoration(
               hintText: "Last name",
             ),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter last name';
+              }
+              return null;
+            },
           ),
           const SizedBox(height: 8),
           TextFormField(
             controller: _mobileTEController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             keyboardType: TextInputType.phone,
             decoration: const InputDecoration(
               hintText: "Mobile",
             ),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter mobile number';
+              }
+              return null;
+            },
           ),
           const SizedBox(height: 8),
           TextFormField(
             controller: _passwordTEController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             decoration: const InputDecoration(
               hintText: "Password",
             ),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter your password';
+              }
+              return null;
+            },
           ),
           const SizedBox(height: 24),
-          ElevatedButton(
-            onPressed: _onTapNextButton,
-            child: const Icon(Icons.arrow_circle_right_outlined),
+          Visibility(
+            visible: !_inProgress,
+            replacement: const CenteredCircularProgressIndicator(),
+            child: ElevatedButton(
+              onPressed: _onTapNextButton,
+              child: const Icon(Icons.arrow_circle_right_outlined),
+            ),
           ),
         ],
       ),
@@ -121,9 +166,56 @@ class _SignUpScreenState extends State<SignUpScreen> {
     );
   }
 
-  void _onTapNextButton() {}
+  void _onTapNextButton() {
+    if (_formKey.currentState!.validate()) {
+      _signUp();
+    }
+  }
+
+  Future<void> _signUp() async {
+    _inProgress = true;
+    setState(() {});
+    Map<String, dynamic> requestBody = {
+      "email" : _emailTEController.text.trim(),
+      "firstName" : _firstNameTEController.text.trim(),
+      "lastName" : _lastNameTEController.text.trim(),
+      "mobile" : _mobileTEController.text.trim(),
+      "password" : _passwordTEController.text,
+      "photo" : "",
+    };
+    NetworkResponse response = await NetworkCaller.postRequest(
+        url: Urls.registration,
+      body: requestBody,
+    );
+    _inProgress = false;
+    setState(() {});
+    if (response.isSuccess) {
+      _clearTextFields();
+      showSnackBarMessage(context, 'New user created');
+    }else{
+      showSnackBarMessage(context, response.errorMessage, true);
+    }
+  }
+
+  void _clearTextFields() {
+    _emailTEController.clear();
+    _firstNameTEController.clear();
+    _lastNameTEController.clear();
+    _mobileTEController.clear();
+    _passwordTEController.clear();
+  }
 
   void _onTapSignInButton() {
     Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _firstNameTEController.dispose();
+    _lastNameTEController.dispose();
+    _mobileTEController.dispose();
+    _passwordTEController.dispose();
+    super.dispose();
   }
 }
